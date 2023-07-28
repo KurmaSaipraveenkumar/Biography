06523da2617b27277324b20e02d52694faeb49ca
KurmaSaipraveenkumar <121428946+KurmaSaipraveenkumar@users.noreply.github.com>
2023-07-28 16:03:53 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,24 @@
+ReactDOM.render(
+     <div>
+        <h1>S.P.Balasubrahmanyam</h1>
+<img src="https://assets.thehansindia.com/h-upload/2023/06/04/1600x960_1354952-spb.jpg" width="700px"></img>
+<h2>Born:4 June 1946</h2>
+<h2>Doed:25 September 2020</h2>
+<ol>
+            <li>Telugu Songs</li>
+            <li>Malaylam Songs</li>
+            <li>Hindi Songs</li>
+             
+            </ol>
+            <p>Balasubrahmanyam started his singing career in Dec 15, 1966, with Sri Sri Sri Maryada Ramanna, a film scored by his mentor Kodandapani. He has sung in more than 5 different Indian languages, including Telugu, Tamil, Kannada, Hindi and Malayalam; more than 40,000 songs since then. He holds the world record in the Guinness Book of World Records for having sung the most number of song recordings by any singer.</p>
+            <h3>Awards</h3>
+            <ol>
+                <li>Padma shri:2001</li>
+                <li>Padma vibhushan:2021</li>
+            </ol>
+                 
+
+            
+
+     </div>,document.getElementById("Biography")
+)
